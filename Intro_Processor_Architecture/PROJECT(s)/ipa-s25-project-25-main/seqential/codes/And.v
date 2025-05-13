c0665c5fffd77f5c667bb4262c65dc296bdcf110
KarthiK_Ande_22 <karthik_ande_22@pop-os.localdomain>
2025-05-13 10:51:15 +0530

    Sem_2-2

--- Intro_Processor_Architecture/PROJECT(s)/ipa-s25-project-25-main/seqential/codes/And.v
+++
@@ -1,9 +0,0 @@
-module AND_64_bit (input [63:0] a, input [63:0] b, output [63:0] and_ab);
-    genvar i;
-    generate
-        for (i = 0; i < 64; i = i + 1) begin
-            and(and_ab[i], a[i], b[i]);
-        end
-        endgenerate
-
-endmodule
